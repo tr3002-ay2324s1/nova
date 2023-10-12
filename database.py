1a34c5a47aab4801f297f6a4050fb84087c0c748
Stuart Long Chay Boon <chayboon@gmail.com>
2023-10-12 16:47:09 -0400

    Add functions for tasks db

--- database.py
+++ database.py
@@ -6,12 +6,13 @@ load_dotenv()
 
 API_URL = os.getenv("API_URL")
 API_KEY = os.getenv("API_KEY")
+
 if not API_URL or not API_KEY:
-    raise Exception("Supabase API_URL or API_KEY not found in .env file")
+    raise Exception("API_URL or API_KEY not found in .env file")
+
 supabase = create_client(API_URL, API_KEY)
 
-
-def add_user(telegram_user_id, username, name, email, google_refresh_token):
+def add_user(telegram_user_id, username, name, google_refresh_token, email=""):
     data = {
         "telegram_user_id": telegram_user_id,
         "username": username,
@@ -40,3 +41,35 @@ def update_user(telegram_user_id, data):
 
 def delete_user(telegram_user_id):
     supabase.table("Users").delete().eq("telegram_user_id", telegram_user_id).execute()
+
+
+def add_task(telegram_user_id, name, description=""):
+    data = {
+        "telegram_user_id": telegram_user_id,
+        "name": name,
+        "description": description
+    }
+    supabase.table("Tasks").insert(data).execute()
+
+
+def fetch_tasks(telegram_user_id):
+    return (
+        supabase.table("Tasks")
+        .select("*")
+        .eq("telegram_user_id", telegram_user_id)
+        .eq("added", False)
+        .execute()
+        .data
+    )
+
+
+def mark_task_as_added(task_id):
+    supabase.table("Tasks").update({
+        "added": True
+    }).eq(
+        "id", task_id
+    ).execute()
+
+
+def delete_task(task_id):
+    supabase.table("Tasks").delete().eq("task_id", task_id).execute()
