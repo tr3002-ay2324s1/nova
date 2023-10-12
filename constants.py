092be8cc1208043a694ff68e0807b246e044711e
Vishnu Sundaresan <68603272+juliussneezer04@users.noreply.github.com>
2023-10-12 00:27:36 -0400

    add new render link

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 import os
 
-BASE_URL = "https://render.something" if os.getenv("ENVIRONMENT") != "dev" else "http://127.0.0.1:8000"
+BASE_URL = "https://tele-bot-server.onrender.com:10000" if os.getenv("ENVIRONMENT") != "dev" else "http://127.0.0.1:8000"
